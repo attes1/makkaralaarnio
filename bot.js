6247553091968d230181b4ab2125e831d0a2d565
Atte Saarinen <atte.saarinen@vincit.fi>
2017-11-24 20:16:57 +0200

    First "working" bot implemantation

--- bot.js
+++ bot.js
@@ -7,35 +7,63 @@ const inBounds = (loc, edge) => {
     loc.z > 0 && loc.z < edge;
 };
 
-const hasBomb = (loc, items) => {
-  return !!items.filter((item) => {
-    return loc.x === item.x && loc.y === item.y && loc.z === item.z;
+const isAvoidableLocation = (loc, locations) => {
+  return !locations || !!locations.filter((l) => {
+    return loc.x === l.x && loc.y === l.y && loc.z === l.z;
   }).length;
 };
 
-const hasPlayer = (loc, players) => {
-  return !!players.filter((player) => {
-    return loc.x === player.x && loc.y === player.y && loc.z === player.z;
-  }).length;
-};
-
-const resolvePossibleMoves = (state, player) => {
-  const locations = [
-    {x: player.x + 1, y: player.y + 0, z: player.z + 0},
-    {x: player.x - 1, y: player.y + 0, z: player.z + 0},
-    {x: player.x + 0, y: player.y + 1, z: player.z + 0},
-    {x: player.x + 0, y: player.y - 1, z: player.z + 0},
-    {x: player.x + 0, y: player.y + 0, z: player.z + 1},
-    {x: player.x + 0, y: player.y + 0, z: player.z - 1}
-  ].filter(loc => inBounds(loc, state.gameInfo.edgeLength) && !hasBomb(loc, items) && !hasPlayer(loc, players));
+const resolvePossibleMoves = (state, player, extraAvoidableLocations) => {
+  return locations = [
+    {x: player.x + 1, y: player.y + 0, z: player.z + 0, direction: '+X'},
+    {x: player.x - 1, y: player.y + 0, z: player.z + 0, direction: '-X'},
+    {x: player.x + 0, y: player.y + 1, z: player.z + 0, direction: '+Y'},
+    {x: player.x + 0, y: player.y - 1, z: player.z + 0, direction: '-Y'},
+    {x: player.x + 0, y: player.y + 0, z: player.z + 1, direction: '+Z'},
+    {x: player.x + 0, y: player.y + 0, z: player.z - 1, direction: '-Z'}
+  ].filter(loc => inBounds(loc, state.gameInfo.edgeLength) &&
+    !isAvoidableLocation(loc, state.items) &&
+    !isAvoidableLocation(loc, state.players) &&
+    !isAvoidableLocation(loc, extraAvoidableLocations));
 };
 
 const getNextTicks = (state) => {
-  const laarnio = state.players.filter((p) => p.name === 'laarnio')[0];
-  const ownMoves = resolvePossibleMoves(state, laarnio);
-};
+  const laarnio = state.players.filter((p) => p.name === state.currentPlayer.name)[0];
+  const otherPlayersPossibleMoves = [].concat(state.players.filter((p) => p.name === state.currentPlayer.name).map((p) => {
+    return resolvePossibleMoves(state, p).push({x: p.x, y: p.y, z: p.z});
+  }));
 
-//getNextTicks(state);
+  const ownPossibleMoves = resolvePossibleMoves(state, laarnio, otherPlayersPossibleMoves);
+
+  // const maxCost = 10000;
+  // let moveCost = 0;
+  // let bombCost = 0;
+  // let noopCost = 1000;
+
+  // moveCost = numOfTasksPerTick * 100;
+  // bombCost = 100 / numOfTasksPerTick;
+
+  const tasks = [];
+
+  for (let i = 0; i < state.gameInfo.numOfTasksPerTick; i++) {
+    const pm = ownPossibleMoves[0];
+
+    if (pm) {
+      tasks.push({
+        task: 'MOVE',
+        direction: pm.direction
+      });
+    } else {
+      tasks.push({
+        task: 'NOOP'
+      });
+    }
+  }
+
+  console.log(tasks);
+
+  return tasks;
+};
 
 http.createServer((req, res) => {
   if (req.method === 'POST') {
@@ -46,10 +74,10 @@ http.createServer((req, res) => {
     });
 
     req.on('end', () => {
-      const nextTickInfo = JSON.parse(body);
+      const gameState = JSON.parse(body);
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(getNextTicks(gameState)));
     });
 
   }
-}).listen(1337);
+}).listen(4001);
